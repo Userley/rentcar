2b30a238fda61bed5b68aceb2fef434683e0208a
Erick Leyva Díaz <userley.diaz@gmail.com>
2023-04-20 00:20:22 -0500

    actualización diseño vehículo

--- resources/views/vehiculo/crear.blade.php
+++ resources/views/vehiculo/crear.blade.php
@@ -39,18 +39,176 @@
                 </div>
                 <div class="ibox-content">
                     <div class="row">
-                        <div class="col-md-6">
-                            a
+                        <div class="col-md-9">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="txtPlaca">Placa:</label>
+                                        <input type="text" class="form-control" id="txtPlaca">
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="txtMarca">Marca:</label>
+                                        <input type="text" class="form-control" id="txtMarca">
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="txtModelo">Modelo:</label>
+                                        <input type="text" class="form-control" id="txtModelo">
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="cboProveedor">Proveedor:</label>
+                                        <select name="" id="cboProveedor" class="form-control"></select>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="txtKilometraje">Kilometraje:</label>
+                                        <input type="text" class="form-control" id="txtKilometraje">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="txtProyecto">Proyecto:</label>
+                                        <input type="text" class="form-control" id="txtProyecto">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="">
+                                        <label for="lugarTrabajo">Lugar Trabajo:</label>
+                                        <div class="input-group mb-3">
+                                            <input type="text" class="form-control" style="height: 34px;"
+                                                id="lugarTrabajo" placeholder="placa" value="CUSCO" disabled>
+                                            <div class="input-group-append">
+                                                <button type="button" class="btn btn-outline-success" data-toggle="modal"
+                                                    data-target="#exampleModal">Cambiar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="txtDescripcion">Descripción:</label>
+                                        <textarea name="" id="txtDescripcion" class="form-control" cols="1" rows="2"></textarea>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md-4">
-                            b
+                        <div class="col-md-3">
+                            <div class="form-group text-center">
+                                <img style="height: 145px;" class="rounded shadow image"
+                                    src="https://s3.us-east-2.amazonaws.com/dealer-inspire-vps-vehicle-images/1f0d-18003616/thumbnails/large/19UUB7F94PA000857/6575a8108bbd401195a8aad82c3cc8b9.jpg"
+                                    id="imagenPrevisualizacion">
+                            </div>
+
+                            <div class="input-group mt-5">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="imgvehicle" accept="image/*">
+                                    <label class="custom-file-label" for="imgvehicle">Choose file</label>
+                                </div>
+                                <div class="input-group-append">
+                                    <button type="button" class="btn btn-primary btn-lg"
+                                        id="inputGroupFileAddon02">Subir</button>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md-2">
-                            c
-                        </div>
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        <button class="btn btn-success">Guardar</button>
+                        <button class="btn btn-secondary">Limpiar</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-4">
+
+                            <div class="form-group">
+                                <label for="cboDepartamento">Departamento:</label>
+                                <select class="custom-select" id="cboDepartamento">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+
+                        </div>
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="cboProvincia">Provincia:</label>
+                                <select class="custom-select" id="cboProvincia">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="cboDistrito">Distrito:</label>
+                                <select class="custom-select" id="cboDistrito">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal" id="saveSite">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@section('ready')
+
+    document.querySelector('.custom-file-input').addEventListener('change', function(e) {
+    var fileName = document.getElementById("imgvehicle").files[0].name;
+    var nextSibling = e.target.nextElementSibling
+    nextSibling.innerText = fileName
+    })
+
+@endsection
+
+
+@section('functions')
+
+    const inputFile = document.querySelector('#imgvehicle');
+    const image = document.querySelector('#imagenPrevisualizacion');
+
+    inputFile.addEventListener('input', async (event) => {
+    let imgblob = await comprimirImagen(inputFile.files[0], 25);
+    let srcimg = URL.createObjectURL(imgblob);
+    base64URL = await encodeFileAsBase64URL(imgblob);
+    image.setAttribute('src', base64URL);
+    });
+
+
 @endsection
